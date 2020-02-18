c6840a744b7f59b25d14386f5f2fd2304a9cff89
camratchford <camratchford@gmail.com>
2020-02-18 13:59:25 -0700

    Genericized ENV fills

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 docker image build -t contest_automator:latest .
 sudo docker run -it --rm \
-        -e FIRST_NAME=Cameron \
-        -e LAST_NAME=Ratchford \
-        -e EMAIL=cjratchford@hotmail.com \
-        -e PHONE=4033899337 \
+        -e FIRST_NAME=Namey \
+        -e LAST_NAME=McNamerson \
+        -e EMAIL=firstname.lastname@domain.tld \
+        -e PHONE=1234567890 \
         -v "${HOME}/contest_automator:/app" \
         --name contest_automator  \
         contest_automator:latest
